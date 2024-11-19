0aa05c3314f150ecd691a96eb6b1701f79c5ff83
MickanoRakotomalala <rakotomalalamickano@gmail.com>
2024-11-19 17:49:49 +0300

    Update

---
+++ intranet-somacou/Scripts/index.js
@@ -0,0 +1,39 @@
+﻿let typed = new Typed(".typing_one", {
+    strings: [
+        "Différents coloris de <br> &nbsp; &nbsp; &nbsp; &nbsp; Draps et Oreillers",
+    ],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true,
+    cursorChar:"", //rendre le curseur invisible
+});
+
+let typedtwo = new Typed(".typing_two", {
+    strings: [
+        "Un large Choix <br> &nbsp; &nbsp; &nbsp; &nbsp; de Couvertures",
+    ],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true,
+    cursorChar: "", //rendre le curseur invisible
+});
+
+let typedthree = new Typed(".typing_three", {
+    strings: [
+        "Divers articles <br> &nbsp; &nbsp; &nbsp; &nbsp; de Soins",
+    ],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true,
+    cursorChar:"", //rendre le curseur invisible
+});
+
+let typedfour = new Typed(".typing_four", {
+    strings: [
+        "Une large Collection <br> &nbsp; &nbsp; &nbsp; &nbsp; pour le Bain",
+    ],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true,
+    cursorChar:"", //rendre le curseur invisible
+});
