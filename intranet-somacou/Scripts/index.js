d51f01596a7f67080010183ad83426e207a07782
MickanoRakotomalala <rakotomalalamickano@gmail.com>
2024-11-20 18:04:30 +0300

    Update

--- intranet-somacou/Scripts/index.js
+++ intranet-somacou/Scripts/index.js
@@ -37,3 +37,31 @@ let typedfour = new Typed(".typing_four", {
     loop: true,
     cursorChar:"", //rendre le curseur invisible
 });
+
+// Active
+const currentLocation = location.href;
+const menuItems = document.querySelectorAll('.nav-active a');
+
+menuItems.forEach(item => {
+    if (item.href === currentLocation) {
+        console.log(currentLocation);
+        menuItems.forEach(i => i.parentElement.classList.remove('active-item'));
+        item.parentElement.classList.add('active-item');
+    }
+})
+
+//Methode 2
+//$(document).ready(function () {
+//    // Récupère l'URL actuelle
+//    var currentUrl = window.location.pathname;
+
+//    // Parcours tous les liens de la navbar
+//    $('.nav-active a').each(function () {
+//        var linkUrl = $(this).attr('href');
+
+//        // Vérifie si le lien correspond à l'URL actuelle
+//        if (currentUrl === linkUrl) {
+//            $(this).addClass('active-item');
+//        }
+//    });
+//});
