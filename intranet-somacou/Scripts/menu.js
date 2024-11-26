9d24c75870c1d1f3a6082d8bd01320d208cf4ff1
MickanoRakotomalala <rakotomalalamickano@gmail.com>
2024-11-26 17:02:47 +0300

    Update menu RH

---
+++ intranet-somacou/Scripts/menu.js
@@ -0,0 +1,10 @@
+﻿const Definition = document.getElementById('Definition');
+const Responsability = document.getElementById('Responsability');
+
+function Definition() {
+    Responsability.classList.remove('col-md-9');
+    Responsability.hidden = true;
+
+    Definition.classList.add('col-md-9');
+    Definition.hidden = false;
+}
