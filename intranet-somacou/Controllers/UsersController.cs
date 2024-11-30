75aa903e4742cee4b8c950e091c02f86f00e8daa
Mickano <rakotomalalamickano@gmail.com>
2024-12-01 00:32:01 +0300

    Update

---
+++ intranet-somacou/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using intranet_somacou.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace intranet_somacou.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: User
+        public ActionResult Users()
+        {
+            return View();
+        }
+
+        public ActionResult CreateUser()
+        {
+            CreateUser createuser = new CreateUser();
+            createuser.Id = 1;
+            createuser.Name = "MICKANO RAKOTOMALALA";
+            createuser.Email = "rakotomalala@gmail.com";
+            createuser.Phone = "0341029531";
+            createuser.Address = "Lot 63 AK";
+            createuser.Password = "1234";
+            createuser.Role = "Chef";
+
+            @ViewBag.Message = "Création compte avec succés";
+            return View(createuser);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
