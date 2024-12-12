ffe5dcc4ab17f7f9761ef6391709eef33a7720cb
MickanoRakotomalala <rakotomalalamickano@gmail.com>
2024-12-12 17:06:27 +0300

    Update

---
+++ intranet-somacou/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using intranet_somacou.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace intranet_somacou.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateUser createUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var context = new AppDbContext())
+                {
+                    createUser.CreatedAt = DateTime.Now;
+                    context.Users.Add(createUser);
+                    context.SaveChanges();
+                }
+
+                TempData["SuccessMessage"] = "Inscription avec succés";
+                return RedirectToAction("Index", "Users");
+            }
+            TempData["ErrorMessage"] = "Model invalid";
+            return View(createUser); //si le modèle est invalide
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
--- intranet-somacou/Controllers/UsersController.cs
+++ intranet-somacou/Controllers/UsersController.cs
@@ -9,39 +9,16 @@ namespace intranet_somacou.Controllers
 {
     public class UsersController : Controller
     {
-        // GET: User
+        private readonly AppDbContext _context;
+        public UsersController()
+        {
+            _context = new AppDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Create(CreateUser createUser)
-        {
-            if (ModelState.IsValid) 
-            {
-                using (var context = new AppDbContext())
-                {
-                    createUser.CreatedAt = DateTime.Now;
-                    context.Users.Add(createUser);
-                    context.SaveChanges();  
-                }
-
-                TempData["SuccessMessage"] = "Inscription avec succés";
-                return View(createUser);
-            }
-            TempData["ErrorMessage"] = "Model invalid";
-            return View(createUser); //si le modèle est invalide
-        }
-
-        public ActionResult Login()
-        {
-            return View();
+            var users = _context.Users.ToList();
+            return View(users);
         }
     }
 }
