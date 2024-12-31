96c92682a71a6dc701bc0d99a98ae2d81cda4d14
Mickano <rakotomalalamickano@gmail.com>
2024-12-31 11:12:57 +0300

    Update

--- intranet-somacou/Controllers/UsersController.cs
+++ intranet-somacou/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,10 +16,36 @@ namespace intranet_somacou.Controllers
             _context = new AppDbContext();
         }
 
-        public ActionResult Index()
+        public ActionResult Index(int page = 1, int pageSize = 5)
         {
-            var users = _context.Users.ToList();
+            var users = _context.Users.OrderBy(u => u.Id)
+                                      .Skip((page - 1) * pageSize)
+                                      .Take(pageSize)
+                                      .ToList();
+
+            var totalUsers = _context.Users.Count();
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = (int)Math.Ceiling((double)totalUsers / pageSize);
+
             return View(users);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "ID utilisateur manquant");
+            }
+
+            var user = _context.Users.FirstOrDefault(u => u.Id == id.Value);
+            if (user == null)
+            {
+                TempData["ErrorMessage"] = "L'utilisateur demandé n'existe pas.";
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Roles = new List<string> { "Admin", "Chef", "User" };
+            return View(user);  
         }
     }
 }
