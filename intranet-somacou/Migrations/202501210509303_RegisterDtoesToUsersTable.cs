57160800720a227f90cf84314358e692bc081954
MickanoRakotomalala <rakotomalalamickano@gmail.com>
2025-01-21 08:12:53 +0300

    Update Table in EF

---
+++ intranet-somacou/Migrations/202501210509303_RegisterDtoesToUsersTable.cs
@@ -0,0 +1,18 @@
+﻿namespace intranet_somacou.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RegisterDtoesToUsersTable : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.RegisterDtoes", newName: "Users");
+        }
+        
+        public override void Down()
+        {
+            RenameTable(name: "dbo.Users", newName: "RegisterDtoes");
+        }
+    }
+}
--- intranet-somacou/Models/RegisterDto.cs
+++ intranet-somacou/Models/RegisterDto.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace intranet_somacou.Models
 {
+    [Table("Users")]
     public class RegisterDto
     {
         public int  Id { get; set; }
